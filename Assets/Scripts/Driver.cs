1d95322e4eca2daf959f0d30ac1ee4d9bb9e134a
Paul Stearns <celestialcoyote@gmail.com>
2023-05-31 19:54:24 -0700

    added rigidbody and colliders to objects

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-	[SerializeField] float steerSpeed = 0.1f;
-	[SerializeField] float moveSpeed = 0.01f;
+	[SerializeField] float steerSpeed = 200.0f;
+	[SerializeField] float moveSpeed = 10.0f;
 
     void Start()
     {
@@ -15,8 +15,8 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-		float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-		float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+		float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+		float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, -steerAmount);
 		transform.Translate(0, moveAmount, 0);
